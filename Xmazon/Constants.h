d80ac232b16749d101975cc5253dff8924726e53
Darkkrye <pierre.boudon.27@gmail.com>
2016-02-11 21:14:11 +0100

    Add Constants file with API access.

---
+++ Xmazon/Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  Xmazon
+//
+//  Created by Pierre Boudon on 11/02/2016.
+//  Copyright © 2016 Pierre Boudon. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+FOUNDATION_EXPORT NSString* const nameAccessAPI;
+FOUNDATION_EXPORT NSString* const idAccessAPI;
+FOUNDATION_EXPORT NSString* const secretAccessAPI;
+
+#endif /* Constants_h */
---
+++ Xmazon/Constants.m
@@ -0,0 +1,15 @@
+//
+//  Constants.m
+//  Xmazon
+//
+//  Created by Pierre Boudon on 11/02/2016.
+//  Copyright © 2016 Pierre Boudon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Constants.h"
+
+
+NSString* const nameAccessAPI = @"3IAM Team 10";
+NSString* const idAccessAPI = @"93857a37-c76d-4a8f-a1ca-15cee061a490";
+NSString* const secretAccessAPI = @"997dd5bd20da2f97658db66d6e336e79662231fc";
