6bd2858f7e7060a5ad04277b3352db84ddac1290
Darkkrye <pierre.boudon.27@gmail.com>
2016-02-11 19:51:30 +0100

    Initial Commit

---
+++ Xmazon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xmazon
+//
+//  Created by Pierre Boudon on 11/02/2016.
+//  Copyright © 2016 Pierre Boudon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
